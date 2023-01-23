eb42a411ef5848904efa5fdd36a287de26853913
ahmetCan7 <ahmetcan423507@gmail.com>
2023-01-23 14:14:51 +0300

    Add items count and price calculation

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -3,6 +3,10 @@ import React from "react";
 import { useSelector } from "react-redux";
 import Card from "../../components/Card";
 import { AppState } from "../../store";
+import {
+  calculateCountOfCartItems,
+  calculateTotalPriceOfCartItems,
+} from "../../utils/cart";
 
 function Cart() {
   const items = useSelector((state: AppState) => state.cart);
@@ -19,6 +23,10 @@ function Cart() {
           </Box>
         ))}
       </Container>
+      <Typography variant="h5" align="center">
+        There are {calculateCountOfCartItems(items)} items and total price is{" "}
+        {calculateTotalPriceOfCartItems(items)} TL
+      </Typography>
     </div>
   );
 }
--- src/utils/cart.ts
+++ src/utils/cart.ts
@@ -1,4 +1,15 @@
 import { Cart } from "../types/cart";
+
 export const calculateCountOfCartItems = (items: Cart[]) => {
   return items.reduce((accumalator, item) => accumalator + item.quantity, 0);
+};
+
+export const calculateTotalPriceOfCartItems = (items: Cart[]) => {
+  return items
+    .reduce(
+      (accumalator, item) =>
+        accumalator + item.quantity * item.product.unitPrice,
+      0
+    )
+    .toFixed(2);
 };
